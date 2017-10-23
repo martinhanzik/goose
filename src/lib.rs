8679ce19b1a3fbdbc41ee1065c8c9a1bd7dc5954
Martin Hanzík <martin@hanzik.com>
2017-10-23 19:21:22 +0200

    Add sanity test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
+extern crate duktape_sys as ffi;
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::ffi::{CStr, CString};
+
     #[test]
-    fn it_works() {
+    fn sanity() {
+        let foo = CString::new("foo").unwrap();
+        let result = CString::new("foo123true").unwrap();
+        unsafe {
+            let ctx = ffi::duk_create_heap_default();
+
+            ffi::duk_push_string(ctx, foo.as_ptr());
+            ffi::duk_push_int(ctx, 123);
+            ffi::duk_push_true(ctx);
+            ffi::duk_concat(ctx, 3);
+            assert_eq!(CStr::from_ptr(ffi::duk_get_string(ctx, -1)).to_bytes(), result.to_bytes());
+            ffi::duk_pop(ctx);
+
+            ffi::duk_destroy_heap(ctx);
+        }
     }
 }
